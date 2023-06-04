8fff34d61a1fcbae36a0ad75ef62da5dc57119bf
soylent <1593860+soylent@users.noreply.github.com>
2023-06-03 21:35:53 -0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by user on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
